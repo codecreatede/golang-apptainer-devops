695a96d07e2133d84c09869b85912e589e9f0623
Gaurav Sablok <codeprog@icloud.com>
2024-09-20 14:27:00 +0200

    codecreatede
    finished writing the code for the complete apptainer from installation to creation of sif files. now starting the debug process and usage.

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -32,6 +34,10 @@ var (
 	appinstallsuid    string
 	appinstallnonsuid string
 	createcontainer   string
+	pathfile          string
+	envfile           string
+	author            string
+	runcom            string
 )
 
 var rootCmd = &cobra.Command{
@@ -64,12 +70,35 @@ func init() {
 		StringVarP(&appinstallnonsuid, "appinstall-nonsuid", "n", "app-non-suid", "install the apptainer non-suid")
 	createCmd.Flags().
 		StringVarP(&createcontainer, "container-install", "c", "create-container", "create the container from the specific directories")
-
+	createCmd.Flags().
+		StringVarP(&pathfile, "pathcommandfile", "f", "path-to-the-command-file", "path to the command file to be used for %run")
+	createCmd.Flags().
+		StringVarP(&envfile, "envfile", "e", "path to the env file", "path to the environment file to be loaded")
+	createCmd.Flags().
+		StringVarP(&runcom, "runcommand", "r", "path to the file containing the run commands", "path to the run commands")
+	createCmd.Flags().
+		StringVarP(&author, "author", "a", "name of the author", "author for the container/apptainer")
 	rootCmd.AddCommand(appCmd)
 	rootCmd.AddCommand(appNCmd)
 	rootCmd.AddCommand(createCmd)
 }
 
+// struct for the commands
+type pathfileStruct struct {
+	commandname string
+	command     string
+}
+
+// environment struct
+type envStruct struct {
+	env string
+}
+
+type runCommand struct {
+	command string
+}
+
+// functions for the appfunc
 func appfunc(cmd *cobra.Command, args []string) {
 	if appinstallsuid == "yes" {
 		appC := exec.Command("sudo", "dnf", "install", "-y", "epel-release")
@@ -84,6 +113,8 @@ func appfunc(cmd *cobra.Command, args []string) {
 		}
 	}
 }
+
+// functions for the appNfunc suid
 
 func appNfunc(cmd *cobra.Command, args []string) {
 	if appinstallsuid == "yes" {
@@ -100,8 +131,66 @@ func appNfunc(cmd *cobra.Command, args []string) {
 	}
 }
 
-func createfunc(cmd *cobra.Command, args []string) {
-	fmt.Println("will create sif images for appatainer")
-}
+// functions for the create containers.
+// it reads the struct and stores each command there for later use also.
 
-// finished coding the installation and now start to code the sif file creation.
+func createfunc(cmd *cobra.Command, args []string) {
+	startline := []string{"Bootsrap: Docker"}
+	image := bufio.NewScanner(os.Stdin)
+	commandE := []pathfileStruct{}
+	envE := []envStruct{}
+	runcommand := []runCommand{}
+
+	fOpen, err := os.Open(pathfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fRead := bufio.NewScanner(fOpen)
+	for fRead.Scan() {
+		line := fRead.Text()
+		commandE = append(commandE, pathfileStruct{
+			commandname: strings.Split(string(line), " ")[0],
+			command:     strings.Split(string(line), " ")[1],
+		})
+	}
+
+	enOpen, err := os.Open(envfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	enRead := bufio.NewScanner(enOpen)
+	for enRead.Scan() {
+		line := enRead.Text()
+		envE = append(envE, envStruct{
+			env: string(line),
+		})
+	}
+
+	rOpen, err := os.Open(runcom)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rRead := bufio.NewScanner(rOpen)
+	for rRead.Scan() {
+		line := rRead.Text()
+		runcommand = append(runcommand, runCommand{
+			command: string(line),
+		})
+	}
+	fmt.Println(startline)
+	fmt.Println("From:", image)
+	fmt.Println("%post")
+	for i := range commandE {
+		fmt.Println(commandE[i])
+	}
+	fmt.Println("%environment")
+	for i := range envE {
+		fmt.Println(envE[i])
+	}
+	fmt.Println()
+	fmt.Println("%runscript")
+	for i := range runcommand {
+		fmt.Println(runcommand[i])
+	}
+	fmt.Println(author)
+}
